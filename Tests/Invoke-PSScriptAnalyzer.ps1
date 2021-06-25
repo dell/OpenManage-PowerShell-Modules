adc4d4c89be653d65199fe98b2de7727c2d18dde
Squillario <Trevor_Squillario@Dell.com>
2021-06-25 13:23:40 -0600

    testing github actions

--- Tests/Invoke-PSScriptAnalyzer.ps1
+++ Tests/Invoke-PSScriptAnalyzer.ps1
@@ -1,5 +1,23 @@
-#Install-Module -Name PSScriptAnalyzer -Scope CurrentUser
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$false, ValueFromPipeline)]
+    [Switch]$ExportCsv
+)
+
+if (Get-Module -ListAvailable -Name PSScriptAnalyzer) {
+    Write-Host "Module exists..."
+}
+else {
+    Write-Host "Installing module..."
+    Install-Module -Name PSScriptAnalyzer -Scope CurrentUser
+}
+
 Import-Module -Name PSScriptAnalyzer
 
-Invoke-ScriptAnalyzer -Path .\DellOpenManage\Private\*.ps1
-Invoke-ScriptAnalyzer -Path .\DellOpenManage\Public\*.ps1
+if ($ExportCsv) {
+    $fields = @("Line", "Column", "ScriptName", "Severity", "Message", "RuleName", "ScriptPath")
+    # -Severity Error,Warning
+    Invoke-ScriptAnalyzer -Path .\DellOpenManage\ -Recurse -ExcludeRule PSAvoidUsingWriteHost,PSUseShouldProcessForStateChangingFunctions | Select-Object -Property $fields | Export-Csv -Path .\Tests\Invoke-PSScriptAnalyzer.csv -NoTypeInformation
+} else {
+    Invoke-ScriptAnalyzer -Path .\DellOpenManage\ -Recurse -ExcludeRule PSAvoidUsingWriteHost,PSUseShouldProcessForStateChangingFunctions
+}
